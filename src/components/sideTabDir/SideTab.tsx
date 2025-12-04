ab3795f027e0d0793246e3ceec2856097266996a
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2025-12-04 19:11:49 +0900

    [#1] feat: 사이드탭 로그인 임시 구현

--- src/components/sideTabDir/SideTab.tsx
+++ src/components/sideTabDir/SideTab.tsx
@@ -6,12 +6,20 @@ import indieIcon from '@/assets/sidTab/Document.svg';
 import myIcon from '@/assets/sidTab/Profile.svg';
 import xIcon from '@/assets/sidTab/Star 8.svg';
 import loginIcon from '@/assets/sidTab/Login.svg';
+import logoutIcon from '@/assets/sidTab/Logout.svg';
 import Image from 'next/image';
+import { useState } from 'react';
 
 interface SideTabProps {
   onClose: () => void;
 }
 export default function SideTab({ onClose }: SideTabProps) {
+  // 임시 로그인 상태 (초기값: 로그아웃 상태)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const handleClick = () => {
+    // 임시로 로그인/로그아웃 토글
+    setIsLoggedIn((prev) => !prev);
+  };
   return (
     <section className="bg-black flex flex-col h-screen w-[307px] gap-5">
       <div className="px-8 py-3 cursor-pointer mt-7" onClick={onClose}>
@@ -43,9 +51,16 @@ export default function SideTab({ onClose }: SideTabProps) {
           <span className="text-white">마이페이지</span>
         </div>
         <hr className="border-t border-gray-700 my-6" />
-        <div className="px-8 py-3 cursor-pointer flex gap-3 hover:bg-gray-800">
-          <Image src={loginIcon} alt="로그인" />
-          <span className="text-white">로그인</span>
+
+        <div
+          className="px-8 py-3 cursor-pointer flex gap-3 hover:bg-gray-800"
+          onClick={handleClick}
+        >
+          <Image
+            src={isLoggedIn ? logoutIcon : loginIcon}
+            alt={isLoggedIn ? '로그아웃' : '로그인'}
+          />
+          <span className="text-white">{isLoggedIn ? '로그아웃' : '로그인'}</span>
         </div>
       </div>
     </section>
