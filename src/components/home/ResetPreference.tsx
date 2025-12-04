673133b603719da4505107aee09062e697b114b1
백승선 <baekjimmy@yonsei.ac.kr>
2025-12-04 21:46:31 +0900

    [#1]refactor: 취향 재설정 버튼 크기 수정

--- src/components/home/ResetPreference.tsx
+++ src/components/home/ResetPreference.tsx
@@ -10,9 +10,11 @@ export default function ResetPreference({ isLoggedIn }: Props) {
   if (isLoggedIn) return null;
 
   return (
-    <div className={`w-[335px] h-[46px] flex bg-[#FF3637] mt-[40px] items-center justify-center rounded-[4px]`}>
-      <div className={`flex overflow-x-auto text-[16px] font-semibold`}>
-        취향 재설정 하러 가기
+    <div className={`w-[335px] h-[46px] flex bg-[#FF3637] mt-[40px] items-center rounded-[4px]`}>
+      <div className={`flex overflow-x-auto text-[16px] ml-[12px] font-semibold`}>
+        <span className={"w-[148px] h-[22px] ml-[12px]"}>
+          취향 재설정 하러 가기
+        </span>
       </div>
       <div className={"w-[16px] h-[16px]"}>
         <Image src={next} alt={next}/>
