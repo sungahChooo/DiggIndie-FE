b5c2569cb84bda7ce13d4cff632613184aeb427e
sungahChooo <170587664+sungahChooo@users.noreply.github.com>
2026-01-04 00:41:12 +0900

    fix: authFindHeader 오타 해결

--- src/app/auth/find/id/page.tsx
+++ src/app/auth/find/id/page.tsx
@@ -1,16 +1,14 @@
+'use client';
 
-'use client'
-
-import AuthFindHeader from "@/components/auth/AuthFindHeader";
+import AuthFindHeader from '@/components/auth/AuthFindHeader';
 import FindID from '@/components/auth/FindID';
 
 export default function FindIDPage() {
-
   return (
     <div className="text-white flex flex-col h-screen bg-black">
       <div className="flex flex-col">
-        <div className={"sticky top-0 z-5"}>
-          <AuthFindHeader title={"아이디 찾기"}/>
+        <div className={'sticky top-0 z-5'}>
+          <AuthFindHeader title={'아이디 찾기'} />
         </div>
         <div className="h-[calc(100vh-100px)] overflow-y-auto bg-black">
           <FindID />
@@ -18,4 +16,4 @@ export default function FindIDPage() {
       </div>
     </div>
   );
-=
+}
--- src/components/auth/AuthFindHeader.tsx
+++ src/components/auth/AuthFindHeader.tsx
@@ -1,7 +1,6 @@
-import Image from "next/image";
-import back from "../../assets/icons/back.svg"
-import close from "../../assets/icons/close.svg"
-import { useRouter } from 'next/navigation'
+import Image from 'next/image';
+import back from '@/assets/common/back.svg';
+import { useRouter } from 'next/navigation';
 
 type props = {
   title: string;
@@ -12,14 +11,16 @@ export default function AuthFindHeader({ title }: props) {
 
   return (
     <div className="w-[375px] h-[96px] flex flex-col items-center font-semibold bg-black px-[20px]">
-      <div className="h-[48px] flex flex-col items-center">
-
-      </div>
       <div className="w-[335px] h-[48px] flex justify-center items-center font-semibold">
-        <Image src={back} alt="logo" width={24} height={24} className={"absolute left-[20px] cursor-pointer"} onClick={() => router.back()}/>
-        <span>
-          { title }
-        </span>
+        <Image
+          src={back}
+          alt="logo"
+          width={24}
+          height={24}
+          className={'absolute left-[20px] cursor-pointer'}
+          onClick={() => router.back()}
+        />
+        <span>{title}</span>
       </div>
     </div>
   );
